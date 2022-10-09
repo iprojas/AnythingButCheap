1ecd02705c78a29420992d398f2b757a6859725f
iprojas <rojas.ip@gmail.com>
2022-10-10 01:12:25 +0300

    update

---
+++ precache-manifest.e929eda4ea4b80d1680d11068730bf46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2343e8168b2b7aa1d24a5b7e70980d89",
+    "url": "/abc/index.html"
+  },
+  {
+    "revision": "9cf92b8bf1472dbb20a2",
+    "url": "/abc/static/css/main.bd0515e7.chunk.css"
+  },
+  {
+    "revision": "dda93aaa15f944b7b604",
+    "url": "/abc/static/js/2.17f9de32.chunk.js"
+  },
+  {
+    "revision": "20a6f33d71d567d1fe0dc1c77a1a22e7",
+    "url": "/abc/static/js/2.17f9de32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf92b8bf1472dbb20a2",
+    "url": "/abc/static/js/main.16682de0.chunk.js"
+  },
+  {
+    "revision": "f39d86f92ce71e4579a0",
+    "url": "/abc/static/js/runtime-main.874aff66.js"
+  }
+]);
